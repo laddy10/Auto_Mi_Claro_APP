ab175d3a7d959b299ed949b5554f721ffde410ac
Gustavo Adolfo Orjuela Orjuela <EC3750J@comcel.com.co>
2025-11-20 16:10:42 -0500

    mantenimiento 20112025

--- src/main/java/tasks/PagosYConsultas/DetalleDeTuPlan/DetalleTuPlanLineaEspecificaPaquetes.java
+++ src/main/java/tasks/PagosYConsultas/DetalleDeTuPlan/DetalleTuPlanLineaEspecificaPaquetes.java
@@ -80,6 +80,7 @@ public class DetalleTuPlanLineaEspecificaPaquetes implements Task {
         ValidarTexto.validarTexto(DESCRIPCION_DEL_PLAN),
         ValidarTexto.validarTexto(FAMILIA_Y_AMIGOS),
         ValidarTexto.validarTexto(APLICACIONES_ELEGIBLES),
+        Scroll.scrollUnaVista(),
         ValidarTexto.validarTexto(GESTIONAR_MI_PLAN),
         ValidarTexto.validarTexto(PAQUETES_ADICIONALES),
         ValidarTexto.validarTexto(ADMINISTRAR_ROAMING));
