390d02b9f934f9c8431f1e1687452d179847a438
Gustavo Adolfo Orjuela Orjuela <EC3750J@comcel.com.co>
2025-12-04 15:19:39 -0500

    mantenimiento 4122025

--- src/main/java/utils/ConstantsMiniVersiones/Versiones.java
+++ src/main/java/utils/ConstantsMiniVersiones/Versiones.java
@@ -24,7 +24,7 @@ public class Versiones {
 
     public static final String MINI_VERSION_CONSULTAR_PQR_CONSTANT = "Ver 1.4.13 (2025-11-11)";
 
-    public static final String MINI_VERSION_DETALLE_DE_TU_PLAN_CONSTANT = "Ver 1.1.105 (2025-12-02)";
+    public static final String MINI_VERSION_DETALLE_DE_TU_PLAN_CONSTANT = "Ver 1.1.113 (2025-12-03)";
 
     public static final String MINI_VERSION_ADQUIRIR_PRODUCTOS_CONSTANT = "Ver 1.0.307 (2025-10-20)";
 
