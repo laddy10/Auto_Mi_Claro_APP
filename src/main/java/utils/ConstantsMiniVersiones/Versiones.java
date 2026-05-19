3b74ca1ea38ca9844abe27c5193d096b52701c72
Gustavo Adolfo Orjuela Orjuela <EC3750J@comcel.com.co>
2026-05-19 15:22:40 -0500

    manetenimientos 190526

--- src/main/java/utils/ConstantsMiniVersiones/Versiones.java
+++ src/main/java/utils/ConstantsMiniVersiones/Versiones.java
@@ -17,7 +17,7 @@ public class Versiones {
   public static final String MINI_VERSION_PAGA_TU_FACTURA_CONSTANT = "Versión 1.1.15 (2026-02-03)";
 
   public static final String MINI_VERSION_RECARGAS_Y_PAQUETES_CONSTANT =
-      "Versión 1.2.16 (2026-04-08)";
+      "Versión 1.2.21 (2026-05-13)";
 
   public static final String MINI_VERSION_NECESITAS_AYUDA_CONSTANT = "Versión 1.0.41 (2025-06-25)";
 
--- src/test/java/runners/GeneralRunner.java
+++ src/test/java/runners/GeneralRunner.java
@@ -15,7 +15,7 @@ import utils.DataToFeature;
     glue = {"stepDefinitions", "utils", "hooks", "listeners"},
     snippets = SnippetType.CAMELCASE,
     plugin = {"pretty"},
-    tags = "@SA017")
+    tags = "@PRE106")
 
 public class GeneralRunner {
   @BeforeSuite
