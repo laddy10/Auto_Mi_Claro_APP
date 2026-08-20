c058be3b4315008c7847bcdf7f8cdf6b3e914e6f
Gustavo Adolfo Orjuela Orjuela <EC3750J@comcel.com.co>
2026-08-20 09:59:50 -0500

    mantenimiento inicio de sesión.1

--- src/test/java/runners/GeneralRunner.java
+++ src/test/java/runners/GeneralRunner.java
@@ -15,7 +15,7 @@ import utils.DataToFeature;
     glue = {"stepDefinitions", "utils", "hooks", "listeners"},
     snippets = SnippetType.CAMELCASE,
     plugin = {"pretty"},
-    tags = "@SA009")
+    tags = "@SA006")
 
 public class GeneralRunner {
   @BeforeSuite
